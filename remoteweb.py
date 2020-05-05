45804bc8fcec3110abf7e974ab7d56ea12cc5c91
pstestcase <58517078+PStestCase@users.noreply.github.com>
2020-05-05 14:53:35 -0400

    Update remoteweb.py

--- remoteweb.py
+++ remoteweb.py
@@ -5,25 +5,17 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 driver = webdriver.Chrome()
 url = 'http://localhost:8080/wd/hub'
+descaps = {'browserName': 'chrome', 'loggingPrefs': {'performance': 'INFO'}}
+
+driver = webdriver.Remote(command_executor=url, desired_capabilities=descaps)
+
+driver.get("http://www.youtube.com")
+
+print("Page URL: "+driver.current_url)
+print("Page title: "+driver.title)
+x =driver.title
+
+assert x == "YouTube"
 
 
-
-driver.get('http://codepad.org')
-
-
-python_link = driver.find_elements_by_xpath(
-    "//input[@name='lang' and @value='Python']")[0]
-python_link.click()
-
-
-text_area = driver.find_element_by_id('textarea')
-text_area.send_keys("print 'Hello,' + ' World!'")
-
-
-submit_button = driver.find_element_by_class_name('g-recaptcha')
-submit_button.click()
-
-
-assert "Hello, World!" in driver.get_page_source()
-
-# driver.quit()
+driver.quit()
